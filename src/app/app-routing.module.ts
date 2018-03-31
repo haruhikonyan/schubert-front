732f538e1b04dd2d2399b3ed7deaa14a077ca684
haruhiko <haruhiko1jp@yahoo.co.jp>
2018-03-31 20:32:28 +0900

    各種コンポーネント作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TopComponent } from './top/top.component';
+import { RecruitComponent } from './recruit/recruit.component';
+import { TeamComponent } from './team/team.component';
+import { ConcertComponent } from './concert/concert.component';
+import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
-  { path: '', component: TopComponent}
+  { path: '', component: TopComponent},
+  { path: 'recruit', component: RecruitComponent},
+  { path: 'team', component: TeamComponent},
+  { path: 'concert', component: ConcertComponent},
+  { path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,19 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { TopComponent } from './top/top.component';
+import { RecruitComponent } from './recruit/recruit.component';
+import { TeamComponent } from './team/team.component';
+import { AboutComponent } from './about/about.component';
+import { ConcertComponent } from './concert/concert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopComponent
+    TopComponent,
+    RecruitComponent,
+    TeamComponent,
+    AboutComponent,
+    ConcertComponent
   ],
   imports: [
     AppRoutingModule,
