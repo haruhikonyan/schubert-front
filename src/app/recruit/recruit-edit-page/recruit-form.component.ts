0b690dc556a2e2c577a8f48d97694229e1b44b70
haruhiko <haruhiko1jp@yahoo.co.jp>
2018-04-11 01:13:09 +0900

    WIP recruit 新規作成 from 途中

--- src/app/recruit/recruit-edit-page/recruit-form.component.ts
+++ src/app/recruit/recruit-edit-page/recruit-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { Recruit } from '../recruit.model';
+
 @Component({
   selector: 'app-recruit-form',
   templateUrl: './recruit-form.component.html',
@@ -8,9 +10,13 @@ import { NgForm } from '@angular/forms';
 })
 export class RecruitFormComponent implements OnInit {
 
+  recruit: Recruit;
   constructor() { }
 
   ngOnInit() {
+    // TODO resolve で recruit が取得できなければ Recruit を new するという処理にする
+    // もしくは url 見る？
+    this.recruit = new Recruit();
   }
 
   onSubmit(f: NgForm) {
