f6ffa40d5a8b00755c1a16525f3f41357d5d5590
Ryosuke Kaizawa <ryosuke@weseek.co.jp>
2018-05-11 00:52:32 +0900

    upgrade to Angular6

--- src/app/recruit/recruit.service.ts
+++ src/app/recruit/recruit.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, RequestOptions, Response, Headers,
          URLSearchParams, ResponseContentType } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
 
 import * as urljoin from 'url-join';
