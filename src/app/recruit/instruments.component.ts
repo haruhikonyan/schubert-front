f8c06b5fbfd62e51d425a9bc3e1ba44c4b0aff40
haruhiko <haruhiko1jp@yahoo.co.jp>
2018-05-23 23:58:03 +0900

    instrumentsComponetに名前変更

--- src/app/recruit/instruments.component.ts
+++ src/app/recruit/instruments.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Instrument } from '../app.model';
 
 @Component({
-  selector: 'app-instrument',
-  templateUrl: './instrument.component.html',
-  // styleUrls: ['./instrument.component.scss']
+  selector: 'app-instruments',
+  templateUrl: './instruments.component.html',
+  // styleUrls: ['./instruments.component.scss']
 })
-export class InstrumentComponent implements OnInit {
+export class InstrumentsComponent implements OnInit {
 
   // recruit情報に含まれるinstrumentsをCategory別に分けた時のCategory名のリスト
   get retainCategories(): string[] {
