46029ac32ea5ba6d4f392f1ef2710c0bdf086ad6
haruhiko <haruhiko1jp@yahoo.co.jp>
2018-05-27 23:27:47 +0900

    認証して募集編集画面へ遷移できるように

--- src/app/recruit/recruit-detail-page.component.ts
+++ src/app/recruit/recruit-detail-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Recruit } from './recruit.model';
 import { Instrument } from '../app.model';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-recruit-detail-page',
@@ -16,16 +17,26 @@ export class RecruitDetailPageComponent implements OnInit {
     return Object.keys(this.instrumentsGroupByCategory);
   }
 
-
   recruit: Recruit;
   instrumentsGroupByCategory: any = {};
 
+  password: string;
+  isLoggedIn: boolean;
+
+  /**
+   * ログインに失敗したときに true になりエラーメッセージを表示する
+   */
+  canShowflushMessage: boolean;
+
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService,
   ) { }
 
   ngOnInit() {
     this.recruit = this.route.snapshot.data['recruit'];
+    this.isLoggedIn = this.authService.isLoggedInByTeamId(this.recruit.team.id);
     // 募集楽器をCategoryでgrouping
     this.recruit.instruments.forEach((val: Instrument, index: number, array: Instrument[]) => {
       const datas: string[] = this.instrumentsGroupByCategory[val.instrumentCategory.name] || [];
@@ -33,6 +44,26 @@ export class RecruitDetailPageComponent implements OnInit {
     });
   }
 
+  moveEditRecruitButtonClickHander(): void {
+    // エラー表示を消す
+    this.canShowflushMessage = false;
+    if (this.isLoggedIn) {
+      this.router.navigate(['recruits', this.recruit.id, 'edit']);
+    }
+    else {
+      // 別 team でログイン中の場合もあるので一旦ログアウトさせる
+      this.authService.logout();
 
+      this.authService.login(this.recruit.team.id, this.password)
+      .subscribe((isLoginSuccessful: boolean) => {
+          if (isLoginSuccessful) {
+            this.router.navigate(['recruits', this.recruit.id, 'edit']);
+          }
+        },
+        (error: Error) => {
+          this.canShowflushMessage = true;
+        });
+    }
+  }
 
 }
